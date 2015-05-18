7c03eaad68853bf546232c8dfe29bd5771c353dc
lamsam <lamsam94@mail.ru>
2015-05-18 19:18:53 +0300

    fix log

--- function.py
+++ function.py
@@ -2,7 +2,6 @@
 import MySQLdb
 from classes import *
 import re
-import os
 
 def databaseConnect():
     return MySQLdb.connect(charset='utf8', host="127.0.0.1",
@@ -50,21 +49,32 @@ def create_log_str(zip, file, region, tag):
     str = region + '\t' + 'zip: ' + zip + '\t' + 'file: ' + file + '\t' + 'tag:' + tag
     return str
 
+log_no_value = list() #None
+log_no_tag = list() #'None'
+
 def ContractParse(data, region, zip, file):
+    global log_no_value
+    global log_no_tag
     isPrice = True
+    isRegNum = True
+    isContractID = True
     currContract = Contract()
     currContract.Region = region
     for event,elem in data:
-        if event == 'end' and elem.tag.upper() == 'ID':
-            currContract.id = elem.text.upper()
+        if event == 'end' and elem.tag.upper() == 'ID' and isContractID == True:
+            currContract.id = elem.text
             continue
 
-        if event == 'end' and elem.tag.upper() == 'REGNUM':
-            currContract.RegNum = elem.text.upper()
+        if event == 'start' and elem.tag.upper() == 'PLACER':
+            isRegNum = False
+
+        if event == 'end' and elem.tag.upper() == 'REGNUM' and isRegNum == True:
+            currContract.RegNum = elem.text
             continue
 
         if event == 'end' and elem.tag.upper() == 'NUMBER':
-            currContract.Number = elem.text.upper()
+            currContract.Number = elem.text
+            isRegNum = True
             continue
 
         if event == 'end' and elem.tag.upper() == 'PUBLISHDATE':
@@ -95,6 +105,10 @@ def ContractParse(data, region, zip, file):
             currContract.ProtocolDate = elem.text
             continue
 
+        if event == 'start' and elem.tag.upper() == 'SINGLECUSTOMERREASON':
+            isContractID = False
+            continue
+
         if event == 'start' and elem.tag.upper() == 'CURRENCY':
             currContract.Currency = CurrencyParse(data)
             isPrice = False
@@ -110,45 +124,113 @@ def ContractParse(data, region, zip, file):
             continue
 
         if event == 'end' and elem.tag.upper() == 'CONTRACT':
-            if currContract.id == None or currContract.id == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/id'))
-            if currContract.RegNum == None or currContract.RegNum == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/regNum'))
-            if currContract.Number == None or currContract.Number == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/number'))
-            if currContract.PublishDate == None or currContract.PublishDate == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/publishDate'))
-            if currContract.SignDate == None or currContract.SignDate == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/signDate'))
-            if currContract.NotNumber == None or currContract.NotNumber == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/notificationNumber'))
-            if currContract.Price == None or currContract.Price == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/price'))
+            if currContract.id == None:
+                log_no_value.append('contract/id')
+            elif currContract.id == 'None':
+                log_no_tag.append('contract/id')
+
+            if currContract.RegNum == None:
+                log_no_value.append('contract/regNum')
+            elif currContract.RegNum == 'None':
+                log_no_tag.append('contract/regNum')
+
+            if currContract.Number == None:
+                log_no_value.append('contract/number')
+            elif currContract.Number == 'None':
+                log_no_tag.append('contract/number')
+
+            if currContract.PublishDate == None:
+                log_no_value.append('contract/publishDate')
+            elif currContract.PublishDate == 'None':
+                log_no_tag.append('contract/publishDate')
+
+            if currContract.SignDate == None:
+                log_no_value.append('contract/signDate')
+            elif currContract.SignDate == 'None':
+                log_no_tag.append('contract/signDate')
+
+            if currContract.NotNumber == None:
+                log_no_value.append('contract/notificationNumber')
+            elif currContract.NotNumber == 'None':
+                log_no_tag.append('contract/notificationNumber')
+
+            if currContract.Price == None:
+                log_no_value.append('contract/price')
+            elif currContract.Price == 'None':
+                log_no_tag.append('contract/price')
             #---------------------------------------------------------------
-            if currContract.Customer.RegNum == None or currContract.Customer.RegNum == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/customer/regNum'))
-            if currContract.Customer.FullName == None or currContract.Customer.FullName == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/customer/fullName'))
-            if currContract.Customer.inn == None or currContract.Customer.inn == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/customer/inn'))
-            if currContract.Customer.kpp == None or currContract.Customer.kpp == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/customer/kpp'))
+
+            if currContract.Customer.RegNum == None:
+                log_no_value.append('contract/customer/regNum')
+            elif currContract.Customer.RegNum == 'None':
+                log_no_tag.append('contract/customer/regNum')
+
+            if currContract.Customer.FullName == None:
+                log_no_value.append('contract/customer/fullName')
+            elif currContract.Customer.FullName == 'None':
+                log_no_tag('contract/customer/fullName')
+
+            if currContract.Customer.inn == None:
+                log_no_value.append('contract/customer/inn')
+            elif currContract.Customer.inn == 'None':
+                log_no_tag.append('contract/customer/inn')
+
+            if currContract.Customer.kpp == None:
+                log_no_value.append('contract/customer/kpp')
+            elif currContract.Customer.kpp == 'None':
+                log_no_tag.append('contract/customer/kpp')
+
             #--------------------------------------------------------------
-            if currContract.Supplier.inn == None or currContract.Supplier.inn == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/supplier/inn'))
-            if currContract.Supplier.kpp == None or currContract.Supplier.kpp == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/supplier/kpp'))
-            if currContract.Supplier.OrgName == None or currContract.Supplier.OrgName == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/supplier/organizationName'))
-            if currContract.Supplier.CountryName == None or currContract.Supplier.CountryName == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/supplier/countryFullName'))
-            if currContract.Supplier.FactAddress == None or currContract.Supplier.FactAddress == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/supplier/factualAddress'))
-            if currContract.Supplier.ContactInfo == None or currContract.Supplier.ContactInfo == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/supplier/contactInfo'))
-            if currContract.Supplier.ContactPhone == None or currContract.Supplier.ContactPhone == 'None':
-                log_write(create_log_str(zip, file, region, ' contract/supplier/contactPhone'))
+
+            if currContract.Supplier.inn == None:
+                log_no_value.append('contract/supplier/inn')
+            elif currContract.Supplier.inn == 'None':
+                log_no_tag.append('contract/supplier/inn')
+
+            if currContract.Supplier.kpp == None:
+                log_no_value.append('contract/supplier/kpp')
+            elif currContract.Supplier.kpp == 'None':
+                log_no_tag.append('contract/supplier/kpp')
+
+            if currContract.Supplier.OrgName == None:
+                log_no_value.append('contract/supplier/organizationName')
+            elif currContract.Supplier.OrgName == 'None':
+                log_no_tag.append('contract/supplier/organizationName')
+
+            if currContract.Supplier.CountryName == None:
+                log_no_value.append('contract/supplier/countryFullName')
+            elif currContract.Supplier.CountryName == 'None':
+                log_no_tag.append('contract/supplier/countryFullName')
+
+            if currContract.Supplier.FactAddress == None:
+                log_no_value.append('contract/supplier/factualAddress')
+            elif currContract.Supplier.FactAddress == 'None':
+                log_no_tag.append('contract/supplier/factualAddress')
+
+            if currContract.Supplier.ContactInfo == None:
+                log_no_value.append('contract/supplier/contactInfo')
+            elif currContract.Supplier.ContactInfo == 'None':
+                log_no_tag.append('contract/supplier/contactInfo')
+
+            if currContract.Supplier.ContactPhone == None:
+                log_no_value.append('contract/supplier/contactPhone')
+            elif currContract.Supplier.ContactPhone == 'None':
+                log_no_tag.append('contract/supplier/contactPhone')
+
+            if currContract.Currency.Code == None:
+                log_no_value.append('contract/currency/code')
+            elif currContract.Currency.Code == 'None':
+                log_no_tag.append('contract/currency/code')
+
+            if currContract.Currency.Name == None:
+                log_no_value.append('contract/currency/name')
+            elif currContract.Currency.Name == 'None':
+                log_no_tag.append('contract/currency/name')
+
             return currContract
+
+ContractParse.log_no_value = list() #None
+ContractParse.log_no_tag = list() #'None'
 
 
 
@@ -270,8 +352,10 @@ def CurrencyParse(data):
         if event == 'end' and elem.tag.upper() == 'CODE':
             currCurrency.Code = elem.text
             continue
+
         if event == 'end' and elem.tag.upper() == 'NAME':
             currCurrency.Name = elem.text
             continue
+
         if event == 'end' and elem.tag.upper() == 'CURRENCY':
             return currCurrency
