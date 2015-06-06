1f36065d1252ca1ff42c9347a64757b98680140a
lamsam <lamsam@users.noreply.github.com>
2015-06-06 21:02:29 +0300

    Delete T_CUSTOMER.sql

--- sql_query/T_CUSTOMER.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE  `T_CUSTOMER`
-(
-    `id` bigint auto_increment,
-    `inn` bigint,
-    `reg_num` bigint,
-    `kpp` bigint,
-    `full_name` varchar(200),
-    primary key (`id`)
-);
